87f9b3560cc762d1809065fa8fe2af5c15c70c97
Marcin Warpechowski <warpech@gmail.com>
2015-03-16 13:42:29 +0100

    put ontology map into a separate file

---
+++ Server/OntologyMap.cs
@@ -0,0 +1,14 @@
+﻿/**
+ * Ontology map lives in a separate file to emphasize that mapping  
+ * should be done in an external PeopleMap.exe app or even on the fly
+ */
+using PolyjuiceNamespace;
+
+namespace People {
+    class OntologyMap {
+        static void Register() {
+            Polyjuice.OntologyMap("/people/persons/@w", "/so/person/@w", null, null);
+            Polyjuice.OntologyMap("/people/organizations/@w", "/so/organization/@w", null, null);
+        }
+    }
+}
