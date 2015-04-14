c41bbcf30ddf31432363a940a243ed6e25c7720c
miyconst <const.miyang@gmail.com>
2015-04-14 15:47:35 +0300

    Fix *.bat files, move bin folder to root

--- build.bat
+++ build.bat
@@ -6,9 +6,9 @@ rem call git --git-dir="%~dp0.git" pull || (GOTO ERROR)
 REM call git --git-dir="%~dp0.git" --work-tree="%~dp0" pull && (echo success) || (GOTO ERROR)
 SET FOUND_SOURCE=0
 REM Echo Building Source
-FOR %%i IN (%~dp0*.csproj) DO ( 
+FOR %%i IN (%~dp0src\People\*.csproj) DO ( 
 SET FOUND_SOURCE=1
-REM msbuild %~dp0\src\AppStoreClient.csproj && (GOTO ERROR)
+REM msbuild %~dp0\src\People\People.csproj && (GOTO ERROR)
 msbuild %%i || (GOTO ERROR)
 )
 
